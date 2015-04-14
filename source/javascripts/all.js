6487a2cb6493fe5c46a3286490f5a6c82bdea4c4
Kyndt Clovis <clovis.kyndt@epitech.eu>
2015-04-15 01:06:37 +0800

    put the css in the end

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,27 @@
-//= require_tree .
+var cb = function() {
+  var h = document.getElementsByTagName('head')[0];
+  var foundation = document.createElement('link');
+  foundation.rel = 'stylesheet';
+  foundation.href = '/stylesheets/foundation.css';
+  h.parentNode.insertBefore(foundation, h);
+
+  var normalize = document.createElement('link');
+  normalize.rel = 'stylesheet';
+  normalize.href = '/stylesheets/normalize.css';
+  var h = document.getElementsByTagName('head')[0];
+  h.parentNode.insertBefore(normalize, h);
+
+  var optimized = document.createElement('link');
+  optimized.rel = 'stylesheet';
+  optimized.href = '/stylesheets/optimized.css';
+  h.parentNode.insertBefore(optimized, h);
+
+  var fontawesome = document.createElement('link');
+  fontawesome.rel = 'stylesheet';
+  fontawesome.href = '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css';
+  h.parentNode.insertBefore(fontawesome, h);
+};
+var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+    webkitRequestAnimationFrame || msRequestAnimationFrame;
+if (raf) raf(cb);
+else window.addEventListener('load', cb);
