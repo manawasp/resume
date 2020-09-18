360df3e78dfa94bf2d5c244031bc88bf2f60c4b4
Kyndt Clovis <clovis@numberly.com>
2020-09-18 22:38:59 +0200

    add async + script to clean

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
-awk '{gsub(/<script type="text\/javascript" src="\/build\.js"><\/script>/, "", $0); print}' dist/fr/index.html
-awk '{gsub(/<script type="text\/javascript" src="\/build\.js"><\/script>/, "", $0); print}' dist/cn/index.html
-awk '{gsub(/<script type="text\/javascript" src="\/build\.js"><\/script>/, "", $0); print}' dist/en/index.html
+echo `awk '{gsub(/<script type="text\/javascript" src="\/build\.js"><\/script>/, "", $0); print}' dist/fr/index.html` > dist/fr/index.html
+echo `awk '{gsub(/<script type="text\/javascript" src="\/build\.js"><\/script>/, "", $0); print}' dist/cn/index.html` > dist/cn/index.html
+echo `awk '{gsub(/<script type="text\/javascript" src="\/build\.js"><\/script>/, "", $0); print}' dist/en/index.html` > dist/en/index.html
 mv dist/en/index.html dist/index.html
 rmdir dist/en
