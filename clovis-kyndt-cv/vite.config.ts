bfd44e5efb67ef79d78e860dc9843b7f98ea4300
Clovis Kyndt <clovis.kyndt@gmail.com>
2022-07-15 14:36:38 +0200

    bump repository clean use nuxt

--- clovis-kyndt-cv/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
-import content from '@originjs/vite-plugin-content'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), svgLoader(), content()]
-})
