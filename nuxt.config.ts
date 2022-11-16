155c5f41f2ab394ede8ba574291df1c1bc4b2d8e
Clovis Kyndt <clovis.kyndt@gmail.com>
2022-11-16 12:28:23 +0100

    fix: make build time public

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,9 @@ import svgLoader from 'vite-svg-loader'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   runtimeConfig: {
-    BUILD_TIME: dayjs().format('MMMM D, YYYY'),
+    public: {
+      BUILD_TIME: dayjs().format('MMMM D, YYYY'),
+    },
   },
   vite: {
     plugins: [
