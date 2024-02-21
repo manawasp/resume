14ec69dfb32c398099406b781567ec119df24a42
Clovis Kyndt <clovis.kyndt@gmail.com>
2024-02-21 22:31:05 +0100

    build(deps): bump dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,8 @@ import * as data from '../locales/en.json'
 
 <template>
   <main>
-    <div v-for="(el, idx) in data.resume" :key="idx" :class="`el-${el.type}`">
+    <div v-for="(el, idx) in data.default.resume" :key="idx" :class="`el-${el.type}`">
       <h2>{{ el.title }}</h2>
-      <!-- NOTE(@manawasp): component solution doesn't work, don't know why -->
       <Text v-if="el.type === 'text'" :value="el.value" />
       <Table v-if="el.type === 'table'" :value="el.value" />
       <Block v-if="el.type === 'block'" :value="el.value" />
